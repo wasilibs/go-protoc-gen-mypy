768177b71cd394b6bf5c27b9906f5bc8293ae4e7
Anuraag Agrawal <anuraaga@gmail.com>
2024-03-07 15:50:06 +0900

    Update to latest wazero

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -10,7 +10,6 @@ import (
 	"strings"
 
 	"github.com/tetratelabs/wazero"
-	"github.com/tetratelabs/wazero/experimental/opt"
 	"github.com/tetratelabs/wazero/imports/wasi_snapshot_preview1"
 	"github.com/tetratelabs/wazero/sys"
 	"github.com/wasilibs/go-protoc-gen-mypy/internal/pysite"
@@ -19,7 +18,7 @@ import (
 func Run(name string, cmdArgs []string, wasm []byte, stdin io.Reader, stdout io.Writer, stderr io.Writer, cwd string) int {
 	ctx := context.Background()
 
-	rt := wazero.NewRuntimeWithConfig(ctx, opt.NewRuntimeConfigOptimizingCompiler())
+	rt := wazero.NewRuntime(ctx)
 
 	wasi_snapshot_preview1.MustInstantiate(ctx, rt)
 
